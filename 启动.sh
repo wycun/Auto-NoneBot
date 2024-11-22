287aa4058eb687870c23cb52dd859bf476c05b7c
温玉 <ch81mggy@163.com>
2024-11-22 19:59:34 +0800

    更新 启动.sh

--- 启动.sh
+++ 启动.sh
@@ -114,8 +114,8 @@ while true; do
         3)
             # 安装 pipx
 echo "开始安装 pipx"
-python -m pip install --user pipx
-python -m pipx ensurepath
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
 # 安装 NoneBot-CIL
 echo "开始安装 NB-CIL"
 pipx install nb-cli
