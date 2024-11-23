705b5a0992d0c001dc7a14d7521eb49682ffb5eb
温玉 <ch81mggy@163.com>
2024-11-23 09:20:14 +0800

    Add files via upload

--- 启动.sh
+++ 启动.sh
@@ -116,6 +116,8 @@ while true; do
 echo "开始安装 pipx"
 python -m pip install --user pipx
 python -m pipx ensurepath
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
 # 安装 NoneBot-CIL
 echo "开始安装 NB-CIL"
 pipx install nb-cli
